8ee29464aed436d81768150f34fd1a6fb506cc9e
leo.matt.547 <leo.matt.547@gmail.com>
2020-11-24 21:31:24 +0700

    Push add rusak

--- src/wahana/wahana.c
+++ src/wahana/wahana.c
@@ -46,6 +46,7 @@ Wahana NewWahana(char jenis[], int id, POINT pos)
     TotalPenggunaan(&W) = 0;
     CurrentPendapatan(&W) = 0;
     CurrentPenggunaan(&W) = 0;
+    Rusak(&W) = 0;
     return W;
 }
 
