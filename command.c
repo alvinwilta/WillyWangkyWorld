74cdc1e87457dc61f18d333c134cb2666c21cc99
Alvin Wilta <51949757+alvinwilta@users.noreply.github.com>
2020-11-28 23:55:22 +0700

    small addition

---
+++ command.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "src/point/point.h"
+#include "src/graph/graph.h"
+
+
+//taro ini di main
+int main () {
+    MATRIKS A,B,C,D; //nanti alokasi dengan bacaan dari file
+    Graph G = initGraphMap(A,B,C,D);
+    POINT LokAntrian = MakePOINT(5,2);
+    POINT LokOffice = MakePOINT(8,2);
+}
+
